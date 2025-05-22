91ef4e9ee37b33b0733ee79d758dded2654b93a6
Vladimir Stolyarov <laserjet4l.mgn@gmail.com>
2025-05-22 17:16:04 +0500

    init push

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 10, 3, 4, 4, 1, 1, 5};
+        String[] words = {"Один", "Два", "Три", "Один", "Два"};
+
+        Map<Integer, Integer> countOfNums = count(nums);
+        countOfNums.forEach((k, v) -> System.out.println(k + ": " + v));
+
+        Map<String, Integer> countOfWords = count(words);
+        countOfWords.forEach((k, v) -> System.out.println(k + ": " + v));
+    }
+
+    private static <T> Map<T, Integer> count(T[] array) {
+        Map<T, Integer> countOfElements = new HashMap<>();
+
+        for (T element : array) {
+            countOfElements.put(element, countOfElements.getOrDefault(element, 0) + 1);
+        }
+
+        return countOfElements;
+    }
+}
